39ea1f7a10e4f5b525a4db36240e2571d8006293
schoolsofthought <schoolsofthought@gmail.com>
2017-02-06 21:33:31 -0500

    Let's go

---
+++ functional_tests.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+
+browser = webdriver.Firefox(firefox_binary=FirefoxBinary(
+    firefox_path=r'C:\Program Files\Mozilla Firefox\firefox.exe'
+))
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
