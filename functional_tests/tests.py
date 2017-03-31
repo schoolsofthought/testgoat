fb2b2067d6f6ada437d1f8b111e1bac4aa1a002f
schoolsofthought <schoolsofthought@gmail.com>
2017-03-31 01:56:15 -0400

    updated Geckodriver, fixed functional test staging server url

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -10,21 +10,18 @@ import os
 MAX_WAIT = 10
 
 class NewVisitorTest(StaticLiveServerTestCase):
-	
-	@classmethod
-	def setUpClass(cls):
-		for arg in sys.argv:
-			if 'liveserver' in arg:
-				cls.server_url = 'http://' + arg.split('=')[1]
-				return
-		super().setUpClass()
-		cls.server_url = cls.live_server_url
+
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		staging_server = os.environ.get('STAGING_SERVER')
+		for arg in sys.argv:
+			if 'STAGING_SERVER' in arg:
+				staging_server = arg.split("=")[1]
+			else:
+				staging_server = ""
 		if staging_server:
-			setattr(self, 'live_server_url', 'http://' + staging_server)
+			self.live_server_url = 'http://' + staging_server
+
 
 	def tearDown(self):
 		self.browser.refresh()
@@ -46,7 +43,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#Edith has heard about a cool new online to-do app. 
 		#She goes to check out its homepage
-		self.browser.get(self.server_url)
+		self.browser.get(self.live_server_url)
 
 		#She notices the page title and header mention to-do lists
 
@@ -82,7 +79,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_multiple_users_can_start_lists_at_different_urls(self):
 		#Edith start a new todo list
-		self.browser.get(self.server_url)
+		self.browser.get(self.live_server_url)
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
@@ -100,7 +97,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser = webdriver.Firefox()
 
 		#Francis visits the home page. There is no sign of Edith's list
-		self.browser.get(self.server_url)
+		self.browser.get(self.live_server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -126,10 +123,11 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_layout_and_styling(self):
 		#Edith goes to the home page
-		self.browser.get(self.server_url)
+		self.browser.get(self.live_server_url)
 		self.browser.set_window_size(1024, 768)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
+		print(type(inputbox))
 		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2,
 			512,
 			delta=10
