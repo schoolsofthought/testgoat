58023ce23269445470f5522f13559535942bcad4
schoolsofthought <schoolsofthought@gmail.com>
2017-06-20 11:01:02 -0600

    end of chapter 14: Simple Form

--- lists/views.py
+++ lists/views.py
@@ -10,7 +10,7 @@ def new_list(request):
 	form = ItemForm(data=request.POST)
 	if form.is_valid():
 		list_ = List.objects.create()
-		Item.objects.create(text=request.POST['text'], list=list_)
+		form.save(for_list=list_)
 		return redirect(list_)
 	else:
 		return render(request, 'home.html', {"form": form})
@@ -18,10 +18,9 @@ def new_list(request):
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
 	form = ItemForm()
-
 	if request.method == 'POST':
 		form = ItemForm(data=request.POST)
 		if form.is_valid():
-			Item.objects.create(text=request.POST['text'], list=list_)
+			form.save(for_list=list_)
 			return redirect(list_)
 	return render(request, 'list.html', {'list': list_, "form": form})
