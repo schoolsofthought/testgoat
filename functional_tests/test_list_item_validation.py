7048aa3386a263c09be72497e8ad876f017407c3
schoolsofthought <schoolsofthought@gmail.com>
2017-07-03 13:45:39 -0600

    First subsection of Chapter 16 on dipping toes into Javascript

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -52,8 +52,29 @@ class ItemValidationTest(FunctionalTest):
 
 		#she sees a helpful error message
 		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has-error').text,
+			self.get_error_element().text,
 			"You've already got this in your list"
+		))
+
+	def test_error_messages_are_cleared_on_input(self):
+		#Edith starts a list and causes a validation error:
+		self.browser.get(self.live_server_url)
+		self.get_item_input_box().send_keys('Banter too thick')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Banter too thick')
+		self.get_item_input_box().send_keys('Banter too thick')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+
+		self.wait_for(lambda: self.assertTrue(
+			self.get_error_element().is_displayed()
+		))
+
+		#She starts typing in the input box to clear the error
+		self.get_item_input_box().send_keys('a')
+
+		#She is pleased to see that the error message disappears
+		self.wait_for(lambda: self.assertFalse(
+			self.get_error_element().is_displayed()
 		))
 
 	def wait_for(self, fn):
@@ -65,6 +86,9 @@ class ItemValidationTest(FunctionalTest):
 				if time.time() - start_time > MAX_WAIT:
 					raise e
 				time.sleep(0.5)
+
+	def get_error_element(self):
+		return self.browser.find_element_by_css_selector('.has-error')
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
